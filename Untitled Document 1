a06829ba7474e01c0948972f42e9d40537778519
Rando Avarmaa <avarmaarando@gmail.com>
2018-08-31 15:46:57 +0300

    prax7 done

--- Untitled Document 1
+++
@@ -1,1 +0,0 @@
-asdasdasad
--- src/mouse_reader/src/mouse_reader.cpp
+++ src/mouse_reader/src/mouse_reader.cpp
@@ -20,28 +20,31 @@ int main(int argc, char *argv[])
 
     ros::Publisher pub_mouse = nh.advertise<geometry_msgs::PointStamped>("mouse", 100);
 
+    geometry_msgs::PointStamped msg;
+
+    int descriptor_ = open("/dev/input/event5", O_RDONLY);
+
+    int const BUFFER_SIZE = 64;
+    struct input_event ibuffer[BUFFER_SIZE];
+    int r, events, i;
+
     while(ros::ok())
     {   
-        geometry_msgs::PointStamped msg;
 
-        int descriptor_ = open("dev/input/mice", O_RDONLY,S_IRWXU);
-
-        int const BUFFER_SIZE = 64;
-        struct input_event ibuffer[BUFFER_SIZE];
-        int r, events, i;
 
         // read events to input_event buffer
         r = read(descriptor_, ibuffer, sizeof(struct input_event) * BUFFER_SIZE);
+        printf("%i\n", r);
         msg.header.frame_id = "map";
         msg.header.stamp = ros::Time();
         if( r > 0 )
         {
             events = r / sizeof(struct input_event);				// getting the number of events
-
+            printf("%i\n", events);
             // going through all the read events
             for(i=0; i<events; ++i)
             {
-                
+                printf("for\n");
                 struct input_event ev = ibuffer[i];
 
                 switch(ev.type)				// switch to a case based on the event type
@@ -55,13 +58,13 @@ int main(int argc, char *argv[])
                     case EV_LED:                              // LED event occured (associated with capslock and numlock)
                         break;
                     case EV_ABS:
-
+                        printf("EV_REL: code=0x%04x, value=0x%08x\n", ibuffer[i].code, ibuffer[i].value);
                         
 
                         if(ev.code == 0)
-                            msg.point.x = ev.value;
+                            msg.point.x = (float)ev.value/500;
                         if(ev.code == 1)
-                            msg.point.y = ev.value;
+                            msg.point.y = (float)ev.value/500;
 
                         msg.point.z = 0;
                         break;
